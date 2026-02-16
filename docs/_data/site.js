d2874367407a4456a9f47448c58ab6e3ce241129
alxndrsn <alxndrsn>
2026-02-16 14:21:13 +0000

    read version from package.json

--- docs/_data/site.js
+++ docs/_data/site.js
@@ -1,9 +1,11 @@
+const { version } = require('../../package.json');
+
 module.exports = {
   name: 'PouchDB',
   description: 'PouchDB, the JavaScript Database that Syncs!',
   url: 'http://pouchdb.com',
   baseurl: '',
-  version: '9.0.0',
+  version,
   github: {
     repository_url: 'https://github.com/apache/pouchdb',
   },
